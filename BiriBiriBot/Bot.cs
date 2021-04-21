fa3ace26022c4d6913f53039751d5e498de1ed6d
Omar Chahdi <OmarChahdi@gmail.com>
2021-04-20 23:37:06 -0300

    Initial Commit

---
+++ BiriBiriBot/Bot.cs
@@ -0,0 +1,65 @@
+﻿using BiriBiriBot.Commands;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.EventArgs;
+using DSharpPlus.Interactivity;
+using Newtonsoft.Json;
+using System;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BiriBiriBot
+{
+    public class Bot
+    {
+        public DiscordClient Client { get; private set; }
+        public CommandsNextExtension Commands { get; private set; }
+
+        public async Task RunAsync()
+        {
+            var json = string.Empty;
+            using (var fs = File.OpenRead("config.json"))
+            using (var sr = new StreamReader(fs, new UTF8Encoding(false)))
+                json = await sr.ReadToEndAsync().ConfigureAwait(false);
+
+            var configJson = JsonConvert.DeserializeObject<ConfigJson>(json);
+
+            var config = new DiscordConfiguration
+            {
+                Token = configJson.Token,
+                TokenType = TokenType.Bot,
+                AutoReconnect = true,
+                MinimumLogLevel = Microsoft.Extensions.Logging.LogLevel.Debug,
+            };
+
+            Client = new DiscordClient(config);
+
+            Client.Ready += OnClientReady;
+
+            var commandsConfig = new CommandsNextConfiguration
+            {
+                StringPrefixes = new string[] { configJson.Prefix },
+                EnableDms = false,
+                EnableMentionPrefix = true,
+                DmHelp = true,
+                IgnoreExtraArguments = true,
+                CaseSensitive = false,
+                UseDefaultCommandHandler = true,
+            };
+
+            Commands = Client.UseCommandsNext(commandsConfig);
+
+            Commands.RegisterCommands<BiriBiriComands>();
+
+            await Client.ConnectAsync();
+
+            await Task.Delay(-1);
+        }
+
+        private Task OnClientReady(DiscordClient d, ReadyEventArgs e)
+        {
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ BiriBiriBot/Commands/BiriBiriComands.cs
@@ -0,0 +1,39 @@
+﻿using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace BiriBiriBot.Commands
+{
+    public class BiriBiriComands: BaseCommandModule
+    {
+        [Command("cry")]
+        public async Task Cry(CommandContext ctx, params string[] inputtext)
+        {
+            var totalstring = String.Join(' ', inputtext);
+            await ctx.Channel.SendMessageAsync(iiiiiiii(totalstring)).ConfigureAwait(false);
+        }
+
+        private string iiiiiiii(string inputValue)
+        {
+            var vowels = new char[] { 'a', 'e', 'o', 'u' };
+            var UpperVowels = new char[] { 'A', 'E', 'O', 'U' };
+            var AccentVowels = new char[] { 'á', 'é', 'ó', 'ú' };
+            inputValue = new string(inputValue.ToCharArray().Select(x => vowels.Any(y => y == x) ? 'i' : x).ToArray());
+            inputValue = new string(inputValue.ToCharArray().Select(x => UpperVowels.Any(y => y == x) ? 'I' : x).ToArray());
+            inputValue = new string(inputValue.ToCharArray().Select(x => AccentVowels.Any(y => y == x) ? 'í' : x).ToArray());
+            return inputValue;
+
+            
+        }
+
+
+
+    }
+
+
+}
